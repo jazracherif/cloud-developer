962e22b9ae60ffc423504b415c896fabd588db8d
Cherif Jazra <jazracherif@gmail.com>
2020-07-15 13:10:11 -0700

    create dev branch and add updates

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -8,6 +8,7 @@ import bodyParser from 'body-parser';
 import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
+  console.log('hello')
   await sequelize.addModels(V0MODELS);
   await sequelize.sync();
 
