1e35f72e093049b2ce19002c0150255dfe06efd0
Cherif Jazra <jazracherif@gmail.com>
2020-07-16 13:17:06 -0700

    feat: enable authentication

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/user.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/user.router.ts
@@ -8,12 +8,19 @@ const router: Router = Router();
 router.use('/auth', AuthRouter);
 
 router.get('/', async (req: Request, res: Response) => {
+    res.status(200).send("ok")
 });
 
-router.get('/:id', async (req: Request, res: Response) => {
+router.get('/:id', requireAuth, async (req: Request, res: Response) => {
     let { id } = req.params;
     const item = await User.findByPk(id);
-    res.send(item);
+
+    if (item == null) {
+        res.status(404).send("No user found");
+    }
+    else {
+        res.status(200).send(item);
+    }
 });
 
 export const UserRouter: Router = router;
